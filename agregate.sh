65d7697520d1c61c3cba7c04cd492d095fd0a672
Andrew Hyatt <ahyattdev@icloud.com>
2016-08-09 13:04:55 -0700

    Added top

---
+++ agregate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p debs
+
+allPacakges="top"
+function build_package {
+  package=$1
+  cd "$package"
+  ./build.sh
+  cd ..
+}
+
+if [ -z ${VAR+x} ]; then
+  for package in "$allPacakges"
+  do
+    build_package $package
+  done
+fi
+
+for package in "$@"
+do
+  if [ -d "$package" ]; then
+    echo "Building: $package"
+    build_package "$package"
+  else
+    echo "Package not found: $package"
+  fi
+done
